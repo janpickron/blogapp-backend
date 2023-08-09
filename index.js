0d605bb7b49b971abf140f62dae5a110fdb0cc6a
Janice Pickron <janpickron@yahoo.com>
2023-08-09 12:42:19 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        res.write('Hello World')
+        res.end()
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]))
+        res.end()
+    }
+})
+server.listen(3000)
 
 // allows us to receive JSON objects
 // app.use(express.json)
 
-app.get('/', (req, res) => {
-   res.send("Hello Janice! sending to the browser. ")
-})
+// app.get('/', (req, res) => {
+//    res.send("Hello Janice! sending to the browser. ")
+// })
 
-app.listen(port, () => {
-    console.log(`Console.log - Example app listening on port ${port}!`)
-})
+// app.listen(port, () => {
+//     console.log(`Console.log - Example app listening on port ${port}!`)
+// })
 
-console.log('Console.log here')
+// console.log('Console.log here')
+
+
 
 // const http = require("http")
 // const hostname = "127.0.0.1"
