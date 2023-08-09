88efb5693f451dadcb6c12b67c8ceb40c213484f
Janice Pickron <janpickron@yahoo.com>
2023-08-09 13:23:15 -0400

    update index.js file

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const app = express()
 const port = 3000
 const http = require('http')
 
-const server = http.createServer((req,res) => {
-    if (req.url === '/') {
-        res.write('Hello World')
-        res.end()
-    }
+// const server = http.createServer((req,res) => {
+//     if (req.url === '/') {
+//         res.write('Hello World')
+//         res.end()
+//     }
 
-    if (req.url === '/api/courses') {
-        res.write(JSON.stringify([1, 2, 3]))
-        res.end()
-    }
-})
-server.listen(3000)
+//     if (req.url === '/api/courses') {
+//         res.write(JSON.stringify([1, 2, 3]))
+//         res.end()
+//     }
+// })
+// server.listen(3000)
 
 // allows us to receive JSON objects
 // app.use(express.json)
 
-// app.get('/', (req, res) => {
-//    res.send("Hello Janice! sending to the browser. ")
-// })
+app.get('/', (req, res) => {
+   res.send("Hello Janice! sending to the browser. ")
+})
 
-// app.listen(port, () => {
-//     console.log(`Console.log - Example app listening on port ${port}!`)
-// })
+app.listen(port, () => {
+    console.log(`Console.log - Example app listening on port ${port}!`)
+})
 
-// console.log('Console.log here')
+console.log('Console.log here')
 
 
 
