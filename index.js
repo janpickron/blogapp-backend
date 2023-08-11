1dd9be65c3f8e95308cef845c78b6f9362c7f9ca
Janice Pickron <janpickron@yahoo.com>
2023-08-10 20:10:50 -0400

    updated files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const port = process.env.PORT || 3000;
-const http = require("http");
 const menuData = require("./menu-items.json");
 const fs = require("fs");
 
