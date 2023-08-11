0ebd4250cc8f398318322ab56a11bd2c83b4f69a
Janice Pickron <janpickron@yahoo.com>
2023-08-11 16:25:06 -0400

    update file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 const menuData = require("./menu-items.json");
 const fs = require("fs");
 
